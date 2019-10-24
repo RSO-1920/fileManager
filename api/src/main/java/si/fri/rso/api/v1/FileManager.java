f5d45a327c2e2427148c6ff26bf5a2756650b210
Uroš Zoretič <uz1734@student.uni-lj.si>
2019-10-24 20:28:08 +0200

    file upload multipart work from java and from postman

--- api/src/main/java/si/fri/rso/api/v1/FileManager.java
+++ api/src/main/java/si/fri/rso/api/v1/FileManager.java
@@ -1,10 +1,20 @@
 package si.fri.rso.api.v1;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import si.fri.rso.api.v1.controller.FileManagerController;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/v1")
 public class FileManager extends Application {
-
-
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> resources = new HashSet<Class<?>>();
+        resources.add(MultiPartFeature.class);
+        resources.add(FileManagerController.class);
+        return resources;
+    }
 }
--- api/src/main/java/si/fri/rso/api/v1/controller/FileManagerController.java
+++ api/src/main/java/si/fri/rso/api/v1/controller/FileManagerController.java
@@ -1,23 +1,22 @@
 package si.fri.rso.api.v1.controller;
 
 
-import java.io.*;
+import org.glassfish.jersey.media.multipart.*;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.validation.constraints.NotEmpty;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.glassfish.jersey.media.multipart.MultiPart;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.*;
+import java.io.*;
 
 @ApplicationScoped
 @Path("/upload")
@@ -28,7 +27,7 @@ public class FileManagerController {
     public File getfile(){
 
         //Your local disk path where you want to store the file
-        String uploadedFileLocation = "C:\\Users\\Jan\\Downloads\\SU_63150214.pdf" ;
+        String uploadedFileLocation = "C:\\Users\\urosz\\Downloads\\playlist.m3u8" ;
         System.out.println(uploadedFileLocation);
         // save it
         File  objFile=new File(uploadedFileLocation);
@@ -45,30 +44,32 @@ public class FileManagerController {
     @POST
     @Path("postFile")
     public void postFile() {
+        // String contentType = "multipart/mixed";
         File newFile = getfile();
         MultiPart multiPart = null;
 
         //TODO: PREBERI SERVER URL IZ CONFIGA
-        String serverURL = "http://127.0.0.1:8088/v1/upload/getter";
+        String serverURL = "http://localhost:8088/v1/upload/getter";
         //TODO: KLIČI SERVICE S TRY_CATCH
         try {
             Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
             WebTarget server = client.target(serverURL);
+
             multiPart = new MultiPart();
-
-            FileDataBodyPart zipBodyPart = new FileDataBodyPart("File", newFile,
-                    MediaType.APPLICATION_OCTET_STREAM_TYPE);
-
+            FileDataBodyPart zipBodyPart = new FileDataBodyPart("file", newFile, MediaType.APPLICATION_OCTET_STREAM_TYPE);
             multiPart.bodyPart(zipBodyPart);
 
+            MediaType contentType = MediaType.MULTIPART_FORM_DATA_TYPE;
+            contentType = Boundary.addBoundary(contentType);
+
             Response response = server.request(MediaType.APPLICATION_JSON_TYPE)
-                    .post(Entity.entity(multiPart, MediaType.MULTIPART_FORM_DATA));
+                    .post( Entity.entity(multiPart, contentType) );
             if (response.getStatus() == 200) {
                 String respnse = response.readEntity(String.class);
                 System.out.println(respnse);
             } else {
 
-                System.out.println("Response is not ok");
+                System.out.println("Response is not ok: " + response.getStatus());
             }
         } catch (Exception e) {
             System.out.println("Exception has occured"+ e.getMessage());
@@ -82,6 +83,26 @@ public class FileManagerController {
             }
         }
     }
+
+
+    @POST
+    @Path("getter")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(@NotEmpty
+                               @FormDataParam("file") InputStream uploadedInputStream,
+                               @FormDataParam("file") FormDataContentDisposition fileDetails) {
+
+        System.out.println("\n\n..CandidateServlet.uploadImage()");
+        System.out.println(fileDetails.getFileName());
+
+        writeToFile(uploadedInputStream, fileDetails.getFileName());
+
+        String output = "File uploaded to : " + fileDetails.getFileName();
+
+        return Response.ok("File uploaded = " + fileDetails.getFileName()).build();
+    }
+
 
     private void writeToFile(InputStream uploadedInputStream,
                              String uploadedFileLocation) {
@@ -101,30 +122,6 @@ public class FileManagerController {
             e.printStackTrace();
         }
     }
-
-    @GET
-    @Path("getter")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getFile(
-        @FormDataParam("file") InputStream uploadedInputStream,
-        @FormDataParam("file") FormDataContentDisposition fileDetails) {
-
-        System.out.println(fileDetails.getFileName());
-
-        String uploadedFileLocation = "C:\\Users\\Jan\\Documents\\fileManager\\izpis.pdf";
-
-        // save it
-        writeToFile(uploadedInputStream, uploadedFileLocation);
-
-        String output = "File uploaded to : " + uploadedFileLocation;
-        return Response.status(Response.Status.OK).entity("OKEEEJ").build();
-    }
-
-// save uploaded file to new location
-
-
-
 
 }
 
